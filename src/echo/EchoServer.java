97cc071f5f1c04d51204a171729b901fed374e32
NEW <NEW@10.0.0.148>
2019-05-19 14:12:25 -0400

    added and populated the assignment specified header

--- src/echo/EchoServer.java
+++ src/echo/EchoServer.java
@@ -1,8 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/* 
+Project: Lab 1
+Purpose Details: Creating a simple client/server "echo" application
+Course: IST 411
+Author: Group 3
+Date Developed: 5/16/2019
+Last Date Changed:5/19/2019
+Revision: Updated client/server communication means
+*/
 package echo;
 
 import java.io.BufferedReader;
